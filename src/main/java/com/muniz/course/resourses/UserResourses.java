13d651ce5ec49264db147764fc104cba45d79247
Maelby Muniz <maelbymuniz@gmail.com>
2023-10-12 10:58:30 -0300

    Feat: UserEntity and UserResources

---
+++ src/main/java/com/muniz/course/resourses/UserResourses.java
@@ -0,0 +1,21 @@
+package com.muniz.course.resourses;
+
+import com.muniz.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResourses {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User user = new User(1L, "Maelby", "maelby@b.com", "9 9999-9999", "teste");
+        return ResponseEntity.ok().body(user);
+
+    }
+
+
+}
